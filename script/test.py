9dc88c5c5f8e7f0c4269cc318556a658f7c7f40d
FrankBlood <1062947208@qq.com>
2017-11-01 14:28:53 +0800

    just have a try

---
+++ script/test.py
@@ -0,0 +1,41 @@
+#! /user/bin/evn python
+# -*- coding:utf8 -*-
+
+"""
+test
+======
+
+A class for something.
+
+@author: Guoxiu He
+@contact: guoxiu.he@whu.edu.cn
+@site: https://frankblood.github.io
+@time: 17-11-1下午2:28
+@copyright: "Copyright (c) 2017 Guoxiu He. All Rights Reserved"
+"""
+
+from __future__ import print_function
+from __future__ import division
+
+import os
+import sys
+
+curdir = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.dirname(curdir))
+
+if sys.version_info[0] < 3:
+    reload(sys)
+    sys.setdefaultencoding("utf-8")
+
+
+class test(object):
+    def __init__(self):
+        pass
+
+
+def func():
+    pass
+
+
+if __name__ == "__main__":
+    pass
